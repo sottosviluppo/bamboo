8d5a49fba0e894db73268a0e21a00847f4fb3921
Emanuela Davelli <e.davelli@filcronet.com>
2026-05-29 09:58:29 +0200

    gestione errore 404

---
+++ src/Elcodi/Store/CoreBundle/EventListener/NotFoundRedirectEventListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Elcodi\Store\CoreBundle\EventListener;
+
+use Doctrine\ORM\EntityNotFoundException as DoctrineEntityNotFoundException;
+use Mmoreram\ControllerExtraBundle\Exceptions\EntityNotFoundException as ControllerEntityNotFoundException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\RouterInterface;
+
+class NotFoundRedirectEventListener
+{
+    /** @var RouterInterface */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        file_put_contents(
+            '/var/www/clients/client1/web26/private/404_debug.log',
+            date('Y-m-d H:i:s').' ['.get_class($exception).'] '.$exception->getMessage()."\n",
+            FILE_APPEND
+        );
+
+        if (
+            !($exception instanceof NotFoundHttpException)
+            && !($exception instanceof DoctrineEntityNotFoundException)
+            && !($exception instanceof ControllerEntityNotFoundException)
+        ) {
+            return;
+        }
+
+        $event->setResponse(new RedirectResponse($this->router->generate('store_homepage')));
+    }
+}
---
+++ web/404.php
@@ -0,0 +1,5 @@
+<?php
+
+header('HTTP/1.1 302 Found');
+header('Location: /');
+exit;
